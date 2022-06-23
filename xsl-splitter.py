007cc84ae0ca76d05ce3a3eb42f536744e9f2bde
Bing Han <bing.han@sap.com>
2022-06-23 22:42:32 +0800

    Use GUI to replace command line.

--- xsl-splitter.py
+++ xsl-splitter.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
 
 import os
-import argparse
 from itertools import groupby
 from openpyxl import Workbook, load_workbook
-
-# command line parsing
-parser = argparse.ArgumentParser(description='Optional app description')
-# Required positional argument
-parser.add_argument('source_file', help='源数据文件名称')
-parser.add_argument('col_name', nargs='?', default='汇算单位名称', help='分组列名称，缺省为“汇算单位名称”')
-args = parser.parse_args()
+import PySimpleGUI as sg
 
 def load_source_data(filename):
     workbook = load_workbook(filename=filename)
@@ -29,8 +22,8 @@ def get_col_index(line, pred):
             return i
     raise Exception("找不到指定的列")
 
-def group_data(source_data):
-    unit_name_col = get_col_index(source_data[0], lambda line:args.col_name in line)
+def group_data(source_data, col_name):
+    unit_name_col = get_col_index(source_data[0], lambda line:col_name in line)
     keyfunc = lambda r:r[unit_name_col]
     grouped_data = groupby(sorted(source_data[1:], key=keyfunc), keyfunc)
     target_data = {}
@@ -49,13 +42,57 @@ def write_one_target_xls(filename, header_line, rows):
         s.append(r)
     wb.save(filename=filename)
 
-def write_target_xls(header_line, data):
-    folder_name = "target/" + os.path.splitext(args.source_file)[0]
+def write_target_xls(target_folder, source_file, header_line, data):
+    folder_name = os.path.join(target_folder, os.path.splitext(source_file)[0])
     if not os.path.isdir(folder_name):
         os.makedirs(folder_name)
     for k in data.keys():
         print("正在写入", k, len(data[k]), "行数据")
-        write_one_target_xls(folder_name + "/" + k + ".xlsx", header_line, data[k])
+        target_filename = os.path.join(folder_name, k+'.xlsx')
+        write_one_target_xls(target_filename, header_line, data[k])
 
-data = load_source_data(args.source_file)
-write_target_xls(data[0], group_data(data))
+source_data = None
+
+sg.theme('Dark Grey 13')
+
+def updateColumnCombo(window, cols):
+    window['-column-combo-'].update(values=cols)
+
+layout = [[sg.Text('源数据文件')],
+          [sg.Input(key='-source-filename-', enable_events=True, expand_x=True, disabled=True),
+           sg.FileBrowse(button_text='打开', target='-source-filename-', file_types=(('Excel', '*.xls *.xlsx'),))],
+          [sg.Text('选择用来分组的列')],
+          [sg.Combo([], key='-column-combo-', expand_x=True, enable_events=True, readonly=True)],
+          [sg.Output(expand_x=True, s=(20,30))],
+          [sg.Text('目标文件夹')],
+          [sg.Input(key='-target-folder-', expand_x=True, disabled=True),
+           sg.FolderBrowse(button_text='浏览')],
+          [sg.Button('执行', disabled=True), sg.Button('退出')],
+          ]
+
+window = sg.Window('数据文件分割器', layout, finalize=True)
+
+while True:  # Event Loop
+    event, values = window.read()
+    if event == "-source-filename-":
+        source_path = values['-source-filename-']
+        print("读入" + source_path)
+        source_data = load_source_data(source_path)
+        updateColumnCombo(window, source_data[0])
+        window["-target-folder-"].update(os.path.join(os.path.dirname(source_path), "target"))
+        continue
+    if event == '-column-combo-':
+        if values['-column-combo-'] != None:
+            window['执行'].update(disabled=False)
+        continue
+    if event == sg.WIN_CLOSED or event == '退出':
+        break
+    if event == '执行':
+        print("分割后文件写入文件夹", values['-target-folder-'])
+        write_target_xls(values['-target-folder-'],
+                         os.path.basename(values['-source-filename-']),
+                         source_data[0],
+                         group_data(source_data, values['-column-combo-']))
+        print("文件" + values['-source-filename-'] + " 分割完成.")
+
+window.close()
